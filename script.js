eec941273e5d71ff2af90e12173419f73567cf89
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-09 11:00:09 -0600

    253 JS chaining promises

--- script.js
+++ script.js
@@ -128,3 +128,26 @@ getCountryData('usa');
 */
 
 /////////// CHAINING PROMISES //////////////
+
+// Define the renderCountry function
+const renderCountry = function (data, className = '') {
+  // Implement the rendering logic here
+};
+
+// Define the getCountryData function
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+
+// Call the getCountryData function
+getCountryData('usa');
