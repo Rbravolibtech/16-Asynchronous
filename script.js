638506ffd20101704b58ffa14138e6b85416f815
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-09 15:52:00 -0600

    259 JS  BUILDING SIMPLE PROMISE

--- script.js
+++ script.js
@@ -310,7 +310,7 @@ GOOD LUCK 😀
 // whereAmI(-33.933, 18.474);
 
 /////////// THE EVENT LOOP IN PRACTICE ////////////
-
+/*
 console.log('Test start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved promise 1').then(res => console.log(res));
@@ -321,3 +321,58 @@ Promise.resolve('Resolved promise 2').then(res => {
 });
 
 console.log('Test end');
+*/
+/////////// BUILDING A SIMPLE PROMISE ////////////
+
+const lottery = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening');
+
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIn 🤪');
+    } else {
+      reject(new Error('You loose your MONEY 💩🚽'));
+    }
+  }, 2000);
+});
+
+lotteryPromise
+  .then(res => console.log(res))
+  .catch(err => console.log.error(err));
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));
